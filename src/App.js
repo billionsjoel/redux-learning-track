68c49b57a68a3b9cc28077bb1ce5c420ad80c5f2
Billions Joel <joelatugonza@gmail.com>
2022-02-17 19:37:11 +0300

    Use switch case for multiple dispatch actions

--- src/App.js
+++ src/App.js
@@ -7,29 +7,17 @@ function App() {
 	};
 
 	const reducer = (state = defaultState, action) => {
-		if (action.type === 'CHANGE') {
-			return {
-				state: 10,
-			};
-		} else {
-			return state;
+		
+
+
+
 		}
 	};
 
 	const store = createStore(reducer);
 
-	const setIncreamentAction = () => {
-		return {
-			type: 'INCREAMENT',
-		};
-	};
-	const setDecreamentAction = () => {
-		return {
-			type: 'DECREAMENT',
-		};
-	};
 
-	//store.dispatch(setStateAction());
+
 
 	return <div className="App">{store.getState().state}</div>;
 }
